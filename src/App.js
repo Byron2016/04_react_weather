7e0f0c69e6af6e0366eaaa9ab6e0627e31cf3d46
MiPersona <bgva2005@yahoo.com>
2020-05-21 12:47:29 -0500

    0008-WeatherCard_Paso propiedades props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 import "./App.css";
 import WeatherCard from "./components/weatherCard/components";
 
 function App() {
+  const App = styled.div`
+    margin: 0 auto;
+    color: white;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  `;
   return (
-    <div className="App">
-      <WeatherCard temp={30} />
-      <WeatherCard temp={-20} />
-      <WeatherCard temp={40} />
-      <WeatherCard temp={0} />
-    </div>
+    <App>
+      <WeatherCard temp={30} condition="Clear" />
+      <WeatherCard temp={-20} condition="Snow" />
+      <WeatherCard temp={40} condition="Tornado" />
+      <WeatherCard temp={0} condition="Hail" />
+    </App>
   );
 }
 
