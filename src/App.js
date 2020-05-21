c95ef08cf3b14e3bc12f324d6e03e1a1836b2e24
MiPersona <bgva2005@yahoo.com>
2020-05-21 00:01:21 -0500

    0003-WeatherCard_Estructura

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import './App.css';
+import WeatherCard from './WeatherCard';
 
 function App() {
   return (
     <div className="App">
-
+      <WeatherCard />
     </div>
   );
 }
